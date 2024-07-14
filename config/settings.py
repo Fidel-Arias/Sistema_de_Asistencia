24ac7433a2b8efa24a1c76d13fa59ee645095125
Fidel-Arias <fr06arias16@gmail.com>
2024-07-14 16:27:34 -0500

    realizando alunos ajustes en el proyecto

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'static',
     'Login',
     'Admin',
@@ -129,7 +130,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
