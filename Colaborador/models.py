1d81eb3b0809f5910defc0194a72e5f03878d16c
Fidel-Arias <fr06arias16@gmail.com>
2024-07-16 22:52:58 -0500

    Agregando la tabla referencial al campo idtipo

--- Colaborador/models.py
+++ Colaborador/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from Bloque.models import MaeBloque
 from CongresoJINIS.models import MaeCongresoJinis
+from tipoUsuario.models import MaeTipoUsuario
 
 # Create your models here.
 class MaeColaborador(models.Model):
@@ -9,7 +10,7 @@ class MaeColaborador(models.Model):
     apellido = models.CharField(max_length=30)
     correo = models.CharField(max_length=50)
     contrasenia = models.CharField(max_length=8)
-    idtipo = models.ForeignKey('MaeTipousuario', models.DO_NOTHING, db_column='idtipo')
+    idtipo = models.ForeignKey(MaeTipoUsuario, models.DO_NOTHING, db_column='idtipo')
     idbloque = models.ForeignKey(MaeBloque, models.DO_NOTHING, db_column='idbloque')
     idcongreso = models.ForeignKey(MaeCongresoJinis, models.DO_NOTHING, db_column='idcongreso')
     
