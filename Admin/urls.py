59ee3ccd7a3439e4a0df9aba7e5bca73a94431e3
Fidel-Arias <fr06arias16@gmail.com>
2024-07-22 20:54:09 -0500

    cambios en el sidebar, pagina para registrar las ubicaciones, cambios en los html y en la url, nuevas imagenes

--- Admin/urls.py
+++ Admin/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('registrar_ponencias/', adminView.as_view({'get':'registrar_ponencia', 'post':'registrar_ponencia'}), name='RegistrarPonencia'),
     path('registrar_ponentes/', adminView.as_view({'get':'registrar_ponentes', 'post':'registrar_ponentes'}), name='RegistrarPonentes'),
     path('registrar_congreso/', adminView.as_view({'get':'registrar_congreso', 'post':'registrar_congreso'}), name='RegistrarCongreso'),
+    path('registrar_ubicacion/', adminView.as_view({'get':'registrar_ubicacion', 'post':'registrar_ubicacion'}), name='RegistrarUbicaciones'),
     path('registrar_bloques/', adminView.as_view({'get':'registrar_bloques', 'post':'registrar_bloques'}), name='RegistrarBloques'),
 ]
